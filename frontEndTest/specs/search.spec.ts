28df1d36a83e2a0046ec4d331f7b6fe012c49a47
Mick McCarthy <mccarthy.mick0012@gmail.com>
2017-09-01 11:57:28 +0100

    Front-end test is now parameterised

--- frontEndTest/specs/search.spec.ts
+++ frontEndTest/specs/search.spec.ts
@@ -2,16 +2,29 @@ var expect = require('chai').expect
 import SearchPage from '../pages/search.page'
 
 describe('Search Field testing', () => {
-    it('should show "Test" on the search results page when searching for "Test"', () => {
-        const searchTerm: string = "test";
-        SearchPage.open();
-        SearchPage.searchInput.setValue(searchTerm);
-        SearchPage.langaugeOption.selectByVisibleText("English");
-        SearchPage.submit();
-        const firstHeadingText = browser.getText('#firstHeading').toLowerCase();
-        expect(searchTerm).to.equal(firstHeadingText, "Expected text to be: " + firstHeadingText);
-        browser.click('//a[text()="Deutsch"]');
-        browser.click('//a[text()="English"]');
-        // browser.end();    
-    });
+
+    const languagePairs = [
+        {
+            'primaryLanguage': 'English', 
+            'secondaryLanguage': 'Deutsch'
+        },
+        {
+            'primaryLanguage': 'English', 
+            'secondaryLanguage': 'Italiano'
+        }
+    ];
+        
+    for (let languagePair of languagePairs) {
+        it('should show "Test" on the search results page when searching for "Test", including languages ${languagePair}', () => {
+            const searchTerm: string = "test";
+            SearchPage.open();
+            SearchPage.searchInput.setValue(searchTerm);
+            SearchPage.langaugeOption.selectByVisibleText(languagePair.primaryLanguage);
+            SearchPage.submit();
+            const firstHeadingText = browser.getText('#firstHeading').toLowerCase();
+            expect(searchTerm).to.equal(firstHeadingText, "Expected text to be: " + firstHeadingText);
+            browser.click('//a[text()="' + languagePair.secondaryLanguage + '"]');
+            browser.click('//a[text()="' + languagePair.primaryLanguage + '"]');
+        });
+    }
 });
